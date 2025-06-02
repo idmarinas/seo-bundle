88e231b9f1ba470bc4f6c388d50db03380dfa523
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-06-02 17:33:13 +0200

    feat(attributes): add `SitemapInterface` definition

---
+++ src/Attributes/SitemapInterface.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 29/05/2025, 18:43
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    SitemapInterface.php
+ * @date    29/05/2025
+ * @time    18:43
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Seo\Attributes;
+
+interface SitemapInterface {}

7472e81bc7de038980c4ed9a9627346a53829750
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-06-05 19:30:14 +0200

    feat(sitemap): add `SitemapHandler` class with `getRootSitemap` method using `SaveLoadTrait`

---
+++ src/Service/SitemapHandler.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 05/06/2025, 19:29
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    SitemapHandler.php
+ * @date    05/06/2025
+ * @time    18:45
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Seo\Service;
+
+use Idm\Bundle\Seo\Cache\SitemapInfo;
+use Idm\Bundle\Seo\Traits\Service\SaveLoadTrait;
+use Psr\Cache\InvalidArgumentException;
+
+final class SitemapHandler
+{
+	use SaveLoadTrait;
+
+	/**
+	 * @throws InvalidArgumentException
+	 */
+	public function getRootSitemap (): SitemapInfo
+	{
+		$sitemap = $this->getCachedSitemap('index', true);
+
+		return $sitemap;
+	}
+}

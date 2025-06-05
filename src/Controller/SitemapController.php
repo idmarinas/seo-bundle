7d6095d36a1bea7a4e2052006d6a23f68b201b4a
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-06-05 19:32:01 +0200

    feat(controller): add `SitemapController` with routes for sitemap index and files

---
+++ src/Controller/SitemapController.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 05/06/2025, 19:28
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    SitemapController.php
+ * @date    26/05/2025
+ * @time    20:50
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Seo\Controller;
+
+use Idm\Bundle\Seo\Service\SitemapHandler;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
+
+final class SitemapController extends AbstractController
+{
+	public function __construct (private readonly SitemapHandler $handler) {}
+
+	/**
+	 * @throws InvalidArgumentException
+	 */
+	#[Route('/sitemap.{_format}',
+		name        : 'idm_seo_sitemap_index',
+		requirements: ['_format' => 'xml'],
+		methods     : ['GET'],
+		format      : 'xml',
+	)]
+	#[Cache(maxage: 3600, public: true)]
+	public function index (): Response
+	{
+		$sitemap = $this->handler->getRootSitemap();
+
+		return new Response($sitemap->getDocument()->toString());
+	}
+
+	#[Route('/sitemap/{name}.{_format}',
+		name        : 'idm_seo_sitemap_file',
+		requirements: [
+			'name'    => '[a-zA-Z]+',
+			'_format' => 'xml',
+		],
+		methods     : ['GET'],
+		format      : 'xml'
+	)]
+	#[Cache(maxage: 3600, public: true)]
+	public function sitemap (string $name): Response
+	{
+		$xml = '';
+
+		return new Response($xml);
+	}
+
+	#[Route('/sitemap/{name}.{id}.{_format}',
+		name        : 'idm_seo_sitemap_file_page',
+		requirements: [
+			'name'    => '[a-zA-Z]+',
+			'id'      => Requirement::DIGITS,
+			'_format' => 'xml',
+		],
+		defaults    : ['id' => 0],
+		methods     : ['GET'],
+		format      : 'xml'
+	)]
+	#[Cache(maxage: 3600, public: true)]
+	public function sitemapPage (string $name, int $id): Response
+	{
+		$xml = '';
+
+		return new Response($xml);
+	}
+}

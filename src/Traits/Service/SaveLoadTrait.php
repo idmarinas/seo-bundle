e748f6f01cb97e98c83eeca2f1d9aa206b2110a1
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-06-08 23:09:13 +0200

    refactor(sitemap): replace `SitemapGenerator` constants with `CacheKeyEnum` and `CacheTagEnum`, add `prepareToSave` and `tagCacheItem` methods

--- src/Traits/Service/SaveLoadTrait.php
+++ src/Traits/Service/SaveLoadTrait.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 06/06/2025, 16:11
+ * Last modified by "IDMarinas" on 08/06/2025, 23:05
  *
  * @project IDMarinas Seo Bundle
  * @see     https://github.com/idmarinas/seo-bundle
@@ -20,8 +20,9 @@
 namespace Idm\Bundle\Seo\Traits\Service;
 
 use DateTime;
+use Idm\Bundle\Seo\Cache\CacheKeyEnum;
+use Idm\Bundle\Seo\Cache\CacheTagEnum;
 use Idm\Bundle\Seo\Cache\SitemapInfo;
-use Idm\Bundle\Seo\Service\SitemapGenerator;
 use Idm\Bundle\Seo\Sitemap\SitemapFile;
 use Psr\Cache\CacheException;
 use Psr\Cache\InvalidArgumentException;
@@ -29,14 +30,13 @@ use Symfony\Contracts\Cache\ItemInterface;
 
 trait SaveLoadTrait
 {
-
 	/**
 	 * @throws InvalidArgumentException
 	 */
 	private function getCachedSitemap (string $name, bool $index = false): SitemapInfo
 	{
-		return $this->cache->get(SitemapGenerator::getCacheKey($name), function (ItemInterface $item) use ($name, $index) {
-			$item->tag(SitemapGenerator::getCacheKey($name));
+		return $this->cache->get(CacheKeyEnum::SITEMAP->suffix($name), function (ItemInterface $item) use ($name, $index) {
+			$item->tag($this->tagCacheItem($name, $index));
 
 			return (new SitemapInfo(new DateTime(), new SitemapFile($name, $index)));
 		});
@@ -48,14 +48,47 @@ trait SaveLoadTrait
 	 */
 	private function save (string $name, SitemapInfo $sitemap): void
 	{
-		$item = $this->cache->getItem(SitemapGenerator::getCacheKey($name));
+		$item = $this->cache->getItem(CacheKeyEnum::SITEMAP->suffix($name));
 		$sitemap->setUpdatedAt(new DateTime());
 
-		$item
-			->tag(SitemapGenerator::getCacheKey($name))
-			->set($sitemap)
-		;
+		$item->set($sitemap);
 
 		$this->cache->save($item);
+	}
+
+	/**
+	 * @throws CacheException
+	 * @throws InvalidArgumentException
+	 */
+	private function prepareToSave (string $name, SitemapInfo $sitemap): void
+	{
+		$document = $sitemap->getDocument();
+
+		if ($document->isValid()) {
+			$this->save($name, $sitemap);
+
+			return;
+		}
+
+		$newDocuments = $document->optimize('generateUrl');
+		foreach ($newDocuments as $fileName => $partDocument) {
+			$sitemapInfo = new SitemapInfo(new DateTime(), $partDocument);
+
+			$this->save($fileName, $sitemapInfo);
+		}
+	}
+
+	/** @private */
+	private function tagCacheItem (string $name, bool $index): iterable
+	{
+		yield CacheTagEnum::SITEMAP->suffix($name);
+
+		if ($index) {
+			yield CacheTagEnum::SITEMAP_INDEX->name;
+			yield CacheTagEnum::SITEMAP_INDEX->suffix($name);
+		} else {
+			yield CacheTagEnum::SITEMAP_PAGE->name;
+			yield CacheTagEnum::SITEMAP_PAGE->suffix($name);
+		}
 	}
 }

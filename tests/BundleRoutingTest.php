f8a06e54a452c48f59dcb014b46ee931e1bfe469
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-06-11 13:39:36 +0200

    test(routing): add assertions for new sitemap routes in `BundleRoutingTest`

--- tests/BundleRoutingTest.php
+++ tests/BundleRoutingTest.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 09/06/2025, 21:46
+ * Last modified by "IDMarinas" on 11/06/2025, 13:38
  *
  * @project IDMarinas Seo Bundle
  * @see     https://github.com/idmarinas/seo-bundle
@@ -40,6 +40,8 @@ class BundleRoutingTest extends KernelTestCase
 		$routes = $routeCollection->all();
 
 		$this->assertCount(4, $routes);
-		$this->assertNotNull($routeCollection->get('app_home'));
+		$this->assertNotNull($routeCollection->get('idm_seo_sitemap_index'));
+		$this->assertNotNull($routeCollection->get('idm_seo_sitemap_file'));
+		$this->assertNotNull($routeCollection->get('idm_seo_sitemap_file_page'));
 	}
 }

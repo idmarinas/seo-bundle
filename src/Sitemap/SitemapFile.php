98449971d75493a85637112c3e6ef8d82a78b582
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-06-15 22:37:13 +0200

    fix(sitemap): correct condition for `$index` assignment in `SitemapFile` constructor

--- src/Sitemap/SitemapFile.php
+++ src/Sitemap/SitemapFile.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "idmarinas" on 15/06/2025, 22:20
+ * Last modified by "idmarinas" on 15/06/2025, 22:37
  *
  * @project IDMarinas Seo Bundle
  * @see     https://github.com/idmarinas/seo-bundle
@@ -60,7 +60,7 @@ final class SitemapFile implements Countable
 	 */
 	public function __construct (private string $name, private ?bool $index = null)
 	{
-		$this->index = $index ?? ('index' === $this->name || u($name)->endsWith('.index'));
+		$this->index = $index ?? ('index' === $name || u($name)->endsWith('.index'));
 		$this->document = new DOMDocument('1.0', 'UTF-8');
 		$this->document->formatOutput = true;
 

e15f0a6c147df8c19bd554150e0657e42648a39c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-06-17 14:26:25 +0200

    feat(config): add `excluded_routes` support in sitemap configuration

--- config/definitions.php
+++ config/definitions.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 09/06/2025, 18:34
+ * Last modified by "idmarinas" on 17/06/2025, 13:46
  *
  * @project IDMarinas Seo Bundle
  * @see     https://github.com/idmarinas/seo-bundle
@@ -24,10 +24,24 @@ return function (DefinitionConfigurator $definition): void {
 	$definition
 		->rootNode()
 			->children()
-				->enumNode('default_scheme')
-					->defaultValue('https')
-					->values(['http', 'https'])
-					->info('Default scheme to use for URLs.')
+				->arrayNode('sitemap')
+					->addDefaultsIfNotSet()
+					->children()
+						->enumNode('default_scheme')
+							->defaultValue('https')
+							->values(['http', 'https'])
+							->info('Default scheme to use for URLs.')
+						->end()
+						->arrayNode('excluded_routes')
+							->info('List of route names or route name patterns to exclude from sitemap. You can exclude specific routes by their exact name or use patterns like "admin_" to exclude all routes starting with that prefix. This helps optimize sitemap generation.')
+							->scalarPrototype()->end()
+							->defaultValue(['_preview_error', '_profiler', '_wdt', '_debug', '_error', 'admin_'])
+							->beforeNormalization()
+								->castToArray()
+								->then(static fn (array $v) => array_unique($v + ['_preview_error', '_profiler', '_wdt', '_debug', '_error']))
+							->end()
+						->end()
+					->end()
 				->end()
 			->end()
 		->end()

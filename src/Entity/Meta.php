a648f223d556440bba77617f67b7c01eb4c887b8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-06 15:31:32 +0100

    refactor(entity): rename `Seo` to `Meta` and update properties with validation constraints

---
+++ src/Entity/Meta.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 06/11/2025, 15:29
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    Meta.php
+ * @date    21/10/2025
+ * @time    17:55
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Seo\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Embeddable]
+class Meta
+{
+	#[ORM\Column]
+	#[Assert\Length(min: 1, max: 160)]
+	#[Assert\NotBlank(allowNull: false)]
+	public string $title;
+
+	#[ORM\Column]
+	#[Assert\NotNull]
+	public string $description = '';
+
+	#[ORM\Column]
+	#[Assert\Url(normalizer: 'trim')]
+	#[Assert\NotNull]
+	public string $canonical = '';
+
+	/**
+	 * @var string[]
+	 */
+	#[ORM\Column(type: Types::SIMPLE_ARRAY)]
+	#[Assert\NotNull]
+	public array $keywords = [];
+
+	/**
+	 * @var string[]
+	 */
+	#[ORM\Column(type: Types::JSON)]
+	public array $robots = [];
+}

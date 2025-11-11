b2bda93ddc4e54ce84521a692573b17f1d80bd96
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-11 16:09:26 +0100

    feat(tests): add fixture for the main app entry point
    - Introduce `index.php` in `tests/Fixtures/app/public` as the app entry point for testing.
    - Set up autoloading and kernel initialization.

---
+++ tests/Fixtures/app/public/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 06/11/2025, 15:44
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    index.php
+ * @date    06/11/2025
+ * @time    15:42
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+use App\Kernel;
+
+require_once dirname(__DIR__, 4) . '/vendor/autoload_runtime.php';
+
+return fn(array $context) => new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);

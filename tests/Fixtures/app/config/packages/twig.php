a8c49440338592694dce9bcda8cedbcf3a731b6c
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-11 16:08:27 +0100

    feat(config): add configuration for WebProfiler, Twig, and TwigComponent
    - Introduce `web_profiler.php` to configure WebProfiler settings for `dev` and `test` environments.
    - Add `twig.php` to specify Twig configuration including default template path and filename pattern.
    - Define `twig_component.php` to set the directory for anonymous Twig components.
    -

---
+++ tests/Fixtures/app/config/packages/twig.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 06/11/2025, 16:20
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    twig.php
+ * @date    06/11/2025
+ * @time    15:48
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Config\TwigConfig;
+
+return static function (TwigConfig $config, ContainerConfigurator $container) {
+	$config
+		->defaultPath(dirname(__DIR__, 2) . '/templates')
+		->fileNamePattern('*.twig')
+	;
+};
---
+++ tests/Fixtures/app/config/packages/twig_component.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 06/11/2025, 17:56
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    twig_component.php
+ * @date    24/03/2025
+ * @time    16:36
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+return static function (ContainerConfigurator $container) {
+	$container->extension('twig_component', [
+		'anonymous_template_directory' => dirname(__DIR__, 2) . '/components/',
+	]);
+};

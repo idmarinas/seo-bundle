b0a31eba83a905b9b26997dbf1358e2ad6c94d95
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-11 16:08:07 +0100

    feat(config): register additional Symfony and third-party bundles
    - Add `EasyAdminBundle`, `DebugBundle`, `SecurityBundle`, `TwigBundle`, `TwigComponentBundle`, `TwigExtraBundle`, and `WebProfilerBundle` to `bundles.php`.

--- tests/Fixtures/app/config/bundles.php
+++ tests/Fixtures/app/config/bundles.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "idmarinas" on 19/03/2025, 18:06
+ * Last modified by "IDMarinas" on 10/11/2025, 16:58
  *
  * @project IDMarinas Seo Bundle
  * @see     https://github.com/idmarinas/seo-bundle
@@ -20,9 +20,16 @@
 use DAMA\DoctrineTestBundle\DAMADoctrineTestBundle;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
+use EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle;
 use Idm\Bundle\Seo\IdmSeoBundle;
+use Symfony\Bundle\DebugBundle\DebugBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\MakerBundle\MakerBundle;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
+use Symfony\UX\TwigComponent\TwigComponentBundle;
+use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
 use Zenstruck\Foundry\ZenstruckFoundryBundle;
 
 return [
@@ -30,10 +37,17 @@ return [
 	DoctrineBundle::class         => ['all' => true],
 
 	// Dev-Test Bundles
+	TwigBundle::class             => ['all' => true],
+	TwigComponentBundle::class    => ['all' => true],
+	TwigExtraBundle::class        => ['all' => true],
+	SecurityBundle::class         => ['all' => true],
 	MakerBundle::class            => ['all' => true],
 	DoctrineFixturesBundle::class => ['all' => true],
 	DAMADoctrineTestBundle::class => ['all' => true],
+	DebugBundle::class            => ['all' => true],
+	WebProfilerBundle::class      => ['all' => true],
 	ZenstruckFoundryBundle::class => ['all' => true],
+	EasyAdminBundle::class        => ['all' => true],
 
 	// This Bundle
 	IdmSeoBundle::class           => ['all' => true],

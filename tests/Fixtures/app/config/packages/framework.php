58abeb780836e71ee5ab438561922aa21813200e
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-13 14:34:12 +0100

    refactor(tests): consolidate framework configuration into a single file
    - Merged separate framework config files (mailer, messenger, router, session, validation) into a single `framework.php` for cleaner structure and maintainability.
    - Removed redundant individual configuration files.

--- tests/Fixtures/app/config/packages/framework.php
+++ tests/Fixtures/app/config/packages/framework.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 10/11/2025, 16:45
+ * Last modified by "IDMarinas" on 13/11/2025, 14:33
  *
  * @project IDMarinas Seo Bundle
  * @see     https://github.com/idmarinas/seo-bundle
@@ -25,17 +25,42 @@ return static function (FrameworkConfig $config) {
 	$config
 		->secret('test')
 		->test(true)
+		->defaultLocale('en')
+		->enabledLocales(['en', 'es'])
 		->httpMethodOverride(false)
 		->handleAllThrowables(true)
+		->phpErrors()->log(true)
 	;
+
 	$config->csrfProtection()->enabled(true);
-	$config->session()->enabled(true)->cookieSecure(true)->handlerId(null)->cookieSamesite('lax');
-	$config->form()->enabled(true)->csrfProtection()->enabled(true);
 	$config->propertyAccess()->enabled(true);
-	$config->phpErrors()->log(true);
+
+	// Session Config
+	$config->session()->enabled(true)->cookieSecure(true)->handlerId(null)->cookieSamesite('lax');
+
+	// Form Config
+	$config->form()->enabled(true)->csrfProtection()->enabled(true);
+
+	// Assets config
 	$config->assets()->enabled(true);
 
+	// Validation Config
 	$config->validation()->enabled(true)->emailValidationMode('html5')->notCompromisedPassword()->enabled(false);
 
-	$config->defaultLocale('en')->enabledLocales(['en', 'es']);
+	// UID config
+	$config->uid()->enabled(true)->defaultUuidVersion(7)->timeBasedUuidVersion(7);
+
+	// Mailer config
+	$mailer = $config->mailer()->enabled(false);
+	$mailer->dsn(getenv('MAILER_DSN') ?? 'null://null');
+	$mailer->envelope()->sender('idm_bundle@test.bundle');
+	$mailer->header('From', 'IDMarinas Seo Bundle <idm_bundle@test.bundle>');
+
+	// Router config
+	$config->router()->enabled(true)->utf8(true);
+
+	// Messenger config
+	$messenger = $config->messenger()->enabled(false);
+	$messenger->transport('sync', 'in-memory://');
+	$messenger->routing('Symfony\\Component\\Mailer\\Messenger\\SendEmailMessage')->senders(['sync']);
 };

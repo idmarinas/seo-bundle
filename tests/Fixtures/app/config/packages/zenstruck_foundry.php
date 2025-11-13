4d3fc377e3d8257e435c41ca400856aee5424b6f
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-13 14:34:37 +0100

    feat(tests): add Zenstruck Foundry configuration for test fixtures
    - Introduced `zenstruck_foundry.php` to configure `ZenstruckFoundryConfig` with auto-refresh disabled and `flushOnce` enabled for test environment.

---
+++ tests/Fixtures/app/config/packages/zenstruck_foundry.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 13/11/2025, 14:15
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    zenstruck_foundry.php
+ * @date    13/11/2025
+ * @time    14:13
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use Symfony\Config\ZenstruckFoundryConfig;
+
+return static function (ZenstruckFoundryConfig $config) {
+	$config->enableAutoRefreshWithLazyObjects(false)->persistence()->flushOnce(true);
+};

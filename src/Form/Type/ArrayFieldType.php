d622ff04fc9264d302779e43e64e6e342d68c3c8
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-19 18:06:45 +0100

    feat(form): add `ArrayFieldType` for handling array-based form fields
    - Introduced `ArrayFieldType` extending `CollectionType` for managing array-based form inputs.
    - Configured default options, including `allow_add`, `allow_delete`, and custom column CSS classes.
    - Utilized `FieldDto` and `FormVars

---
+++ src/Form/Type/ArrayFieldType.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 19/11/2025, 17:54
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    ArrayFieldType.php
+ * @date    19/11/2025
+ * @time    16:57
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Seo\Form\Type;
+
+use EasyCorp\Bundle\EasyAdminBundle\Dto\FieldDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\FormVarsDto;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use Override;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+final class ArrayFieldType extends AbstractType
+{
+	public function getParent (): string
+	{
+		return CollectionType::class;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	#[Override]
+	public function configureOptions (OptionsResolver $resolver): void
+	{
+		$resolver->setDefaults([
+			'entry_options'      => ['label' => false,],
+			'row_attr'           => ['class' => 'field-array'],
+			'empty_data'         => [],
+			'allow_add'          => true,
+			'allow_delete'       => true,
+			'delete_empty'       => true,
+			'column_css_classes' => 'col-md-12',
+		]);
+
+		$resolver->setAllowedTypes('column_css_classes', ['string']);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	#[Override]
+	public function finishView (FormView $view, FormInterface $form, array $options): void
+	{
+		$field = new FieldDto();
+		$field->setFieldFqcn(ArrayField::class);
+
+		if (is_string($options['column_css_classes'])) {
+			$field->setColumns($options['column_css_classes']);
+		}
+
+		$view->vars['ea_vars'] = new FormVarsDto($field);
+	}
+}

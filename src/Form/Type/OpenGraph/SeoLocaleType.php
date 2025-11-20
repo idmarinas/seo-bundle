6ad6ebfca32f3c3c375b356d8045e314a20365bc
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-20 13:23:56 +0100

    refactor(form): rename `LocaleType` to `SeoLocaleType` and refactor inheritance
    - Renamed `LocaleType` to `SeoLocaleType` for better context and clarity.
    - Updated class to extend `AbstractType` instead of `BaseLocaleType`.
    - Added `getParent()` method returning `LocaleType::class`.
    - Refactored `configureOptions` to remove direct parent calls.

--- src/Form/Type/OpenGraph/SeoLocaleType.php
+++ src/Form/Type/OpenGraph/SeoLocaleType.php
@@ -2,14 +2,14 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 19/11/2025, 16:22
+ * Last modified by "IDMarinas" on 20/11/2025, 13:23
  *
  * @project IDMarinas Seo Bundle
  * @see     https://github.com/idmarinas/seo-bundle
  *
- * @file    LocaleType.php
- * @date    10/11/2025
- * @time    14:15
+ * @file    SeoLocaleType.php
+ * @date    20/11/2025
+ * @time    13:17
  *
  * @author  Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
@@ -20,12 +20,13 @@
 namespace Idm\Bundle\Seo\Form\Type\OpenGraph;
 
 use Override;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\ChoiceList\ChoiceList;
-use Symfony\Component\Form\Extension\Core\Type\LocaleType as BaseLocaleType;
+use Symfony\Component\Form\Extension\Core\Type\LocaleType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use function Symfony\Component\String\u;
 
-final  class LocaleType extends BaseLocaleType
+final class SeoLocaleType extends AbstractType
 {
 	private string $pattern;
 
@@ -38,10 +39,17 @@ final  class LocaleType extends BaseLocaleType
 	 * @inheritDoc
 	 */
 	#[Override]
+	public function getParent (): string
+	{
+		return LocaleType::class;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	#[Override]
 	public function configureOptions (OptionsResolver $resolver): void
 	{
-		parent::configureOptions($resolver);
-
 		// Show only enabled locales
 		$resolver->setDefaults([
 			'choice_filter' => ChoiceList::filter($this, fn(?string $locale) => u($locale)->match($this->pattern)),

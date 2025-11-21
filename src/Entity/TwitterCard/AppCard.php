7648248a8f4796cea9dff1aa3d8bb83443282568
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-11-21 17:33:28 +0100

    refactor(entity): add validation callbacks for `AppCard` fields
    - Introduced static methods `doGooglePlayIdValidation` and `doAppleIdValidation` for custom `id` validation.
    - Added regular expression patterns for `googleplay` and `apple` IDs.
    - Replaced `NotBlank` constraint with `NotNull` for `id` and `name` fields.

--- src/Entity/TwitterCard/AppCard.php
+++ src/Entity/TwitterCard/AppCard.php
@@ -2,7 +2,7 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 12/11/2025, 13:56
+ * Last modified by "IDMarinas" on 21/11/2025, 17:27
  *
  * @project IDMarinas Seo Bundle
  * @see     https://github.com/idmarinas/seo-bundle
@@ -22,22 +22,54 @@ namespace Idm\Bundle\Seo\Entity\TwitterCard;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 #[ORM\Embeddable]
 class AppCard
 {
+	public const string PATTERN_APPLE  = '/^\d{3,}$/';
+	public const string PATTERN_GOOGLE = '/^\w+(?:\.\w+)+$/';
+
 	#[ORM\Column(type: Types::STRING)]
 	#[Assert\Length(min: 0, max: 255)]
-	#[Assert\NotBlank(allowNull: false, groups: ['twitter-card-app'])]
+	#[Assert\NotNull(groups: ['twitter-card-app'])]
 	public string $id = '';
 
 	#[ORM\Column(type: Types::STRING)]
 	#[Assert\Length(min: 0, max: 255)]
-	#[Assert\NotBlank(allowNull: false, groups: ['twitter-card-app'])]
+	#[Assert\NotNull(groups: ['twitter-card-app'])]
 	public string $name = '';
 
 	#[ORM\Column(type: Types::STRING)]
 	#[Assert\Length(min: 0, max: 255)]
 	#[Assert\NotNull]
 	public string $url = '';
+
+	public static function doGooglePlayIdValidation (AppCard $appCard, ExecutionContextInterface $context): void
+	{
+		if ('' === $appCard->id) {
+			return;
+		}
+
+		if (!preg_match(self::PATTERN_GOOGLE, $appCard->id)) {
+			$context
+				->buildViolation('idm_seo.twitter_card.app_id_google_must_be_alphanumeric')
+				->addViolation()
+			;
+		}
+	}
+
+	public static function doAppleIdValidation (AppCard $appCard, ExecutionContextInterface $context): void
+	{
+		if ('' === $appCard->id) {
+			return;
+		}
+
+		if (!preg_match(self::PATTERN_APPLE, $appCard->id)) {
+			$context
+				->buildViolation('idm_seo.twitter_card.app_id_apple_must_be_numeric')
+				->addViolation()
+			;
+		}
+	}
 }

09be9064ef21924fd0795bdf0102963aa7d2cd33
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-02 16:17:34 +0100

    feat(traits): add `SanitizerTrait` to handle string sanitization
    - Introduced `SanitizerTrait` with `sanitize` method for removing HTML tags, collapsing whitespaces, and optional truncation.

---
+++ src/Traits/Service/SeoPage/SanitizerTrait.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 01/12/2025, 13:25
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    SanitizerTrait.php
+ * @date    01/12/2025
+ * @time    13:23
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+namespace Idm\Bundle\Seo\Traits\Service\SeoPage;
+
+use function Symfony\Component\String\u;
+
+trait SanitizerTrait
+{
+	protected function sanitize (string $string, int|false $truncate = false): string
+	{
+		$string = u(strip_tags($string))
+			->collapseWhitespace()
+		;
+
+		if (false !== $truncate) {
+			$string->truncate($truncate, '...');
+		}
+
+		return $string->toString();
+	}
+}

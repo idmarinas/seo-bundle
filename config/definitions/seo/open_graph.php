1aae1ecff69d59f60f6af44aab2ba8e29aaa84d9
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-03 18:49:24 +0100

    feat(config): add default configurations for Twitter and Open Graph tags
    - Introduced `twitter.php` for managing Twitter Card configurations including card type, site, and creator details.
    - Added `open_graph.php` for handling Open Graph default configurations such as site name and type.

---
+++ config/definitions/seo/open_graph.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 02/12/2025, 21:04
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    open_graph.php
+ * @date    11/11/2025
+ * @time    14:30
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+use Idm\Bundle\Seo\Entity\OpenGraph;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+return (function (): NodeDefinition {
+	//@formatter:off
+	$treeBuilder = new TreeBuilder('open_graph');
+
+	return $treeBuilder->getRootNode()
+		->info('Default configuration for Open Graph tags.')
+		->children()
+			->scalarNode('site_name')
+				->info('The name which should be displayed for the overall site.')
+				->defaultValue('IDMarinas Seo Bundle')
+			->end()
+			->enumNode('type')
+				->info('The default type of your object as defined by [Open Graph protocol](https://ogp.me/#types).')
+				->values(OpenGraph::TYPE_ALL)
+				->defaultValue('website')
+			->end()
+		->end();
+})();
---
+++ config/definitions/seo/twitter.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 03/12/2025, 12:05
+ *
+ * @project IDMarinas Seo Bundle
+ * @see     https://github.com/idmarinas/seo-bundle
+ *
+ * @file    twitter.php
+ * @date    11/11/2025
+ * @time    14:30
+ *
+ * @author  Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since   1.0.0
+ */
+
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+return (function (): NodeDefinition {
+	//@formatter:off
+	$treeBuilder = new TreeBuilder('twitter');
+
+	return $treeBuilder->getRootNode()
+		->info('Default configuration for Twitter Card tags.')
+		->children()
+			->enumNode('card')
+				->info('The type of card to display.')
+				->values(['summary', 'summary_large_image', 'app', 'player'])
+				->defaultValue('summary')
+			->end()
+			->scalarNode('site')
+				->info('The Twitter site name. It must start with "@".')
+				->defaultValue('')
+				->validate()
+					->ifTrue(static fn ($v) => !str_starts_with($v, '@'))
+					->thenInvalid('The Twitter site name must start with "@".')
+				->end()
+			->end()
+			->scalarNode('creator')
+				->info('The Twitter creator\'s name. It must start with "@".')
+				->defaultValue('')
+				->validate()
+					->ifTrue(static fn ($v) => !str_starts_with($v, '@'))
+					->thenInvalid('The Twitter creator name must start with "@".')
+				->end()
+			->end()
+		->end();
+})();

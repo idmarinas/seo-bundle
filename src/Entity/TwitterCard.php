5706efa64ce6f34ef79c66e4538780fde4c4fb0d
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-08 11:58:16 +0100

    refactor(entity): add normalizers and NotBlank validation for TwitterCard and Player fields
    - Added `trim` normalizers to several `Assert\Length` validators for consistent whitespace handling.
    - Included `NotBlank` constraints to enforce non-empty values in required fields.

--- src/Entity/TwitterCard.php
+++ src/Entity/TwitterCard.php
@@ -2,19 +2,19 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 17/11/2025, 13:49
+ * Last modified by "IDMarinas" on 08/12/2025, 11:57
  *
  * @project IDMarinas Seo Bundle
- * @see     https://github.com/idmarinas/seo-bundle
+ * @see https://github.com/idmarinas/seo-bundle
  *
- * @file    TwitterCard.php
- * @date    04/11/2025
- * @time    18:25
+ * @file TwitterCard.php
+ * @date 04/11/2025
+ * @time 18:25
  *
- * @author  Iván Diaz Marinas (IDMarinas)
+ * @author Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
 namespace Idm\Bundle\Seo\Entity;
@@ -57,14 +57,17 @@ class TwitterCard implements Stringable
 	public string $description = '';
 
 	#[ORM\Column(type: Types::STRING, length: 1000)]
-	#[Assert\Length(min: 0, max: 1000)]
+	#[Assert\Length(min: 0, max: 1000, normalizer: 'trim')]
+	#[Assert\NotNull]
+	public string $site = '';
+
 	#[Assert\Url(normalizer: 'trim')]
 	#[Assert\NotNull]
 	#[Assert\NotBlank(allowNull: false, groups: ['twitter-card-player'])]
 	public string $image = '';
 
 	#[ORM\Column(type: Types::STRING, length: 420)]
-	#[Assert\Length(min: 0, max: 420)]
+	#[Assert\Length(min: 0, max: 420, normalizer: 'trim')]
 	#[Assert\NotNull]
 	public string $imageAlt = '';
 
--- src/Entity/TwitterCard/Player.php
+++ src/Entity/TwitterCard/Player.php
@@ -2,19 +2,19 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 24/11/2025, 18:30
+ * Last modified by "IDMarinas" on 08/12/2025, 11:52
  *
  * @project IDMarinas Seo Bundle
- * @see     https://github.com/idmarinas/seo-bundle
+ * @see https://github.com/idmarinas/seo-bundle
  *
- * @file    Player.php
- * @date    04/11/2025
- * @time    20:00
+ * @file Player.php
+ * @date 04/11/2025
+ * @time 20:00
  *
- * @author  Iván Diaz Marinas (IDMarinas)
+ * @author Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
 namespace Idm\Bundle\Seo\Entity\TwitterCard;
@@ -27,13 +27,16 @@ class Player
 {
 	#[ORM\Column]
 	#[Assert\Url(protocols: ['https'], normalizer: 'trim', groups: ['twitter-card-player'])]
+	#[Assert\NotBlank(allowNull: false, groups: ['twitter-card-player'])]
 	public string $url = '';
 
 	#[ORM\Column]
 	#[Assert\GreaterThanOrEqual(262, groups: ['twitter-card-player'])]
+	#[Assert\NotBlank(allowNull: false, groups: ['twitter-card-player'])]
 	public int $width = 262;
 
 	#[ORM\Column]
 	#[Assert\GreaterThanOrEqual(262, groups: ['twitter-card-player'])]
+	#[Assert\NotBlank(allowNull: false, groups: ['twitter-card-player'])]
 	public int $height = 262;
 }

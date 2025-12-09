e6e4f86d3dcf5ca26b62ffdfa477aba753f81724
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-09 19:16:46 +0100

    feat(subscriber): add `SeoConfigureSubscriber` to handle SEO configuration dynamically
    - Introduced `SeoConfigureSubscriber` to manage SEO properties during controller argument events.
    - Added support for configuring locale, route name, and sitemap data using `Seo` and `Sitemap` attributes.
    - Enabled dynamic entity-based SEO adjustments with attribute-driven logic.

---
+++ src/EventSubscriber/SeoConfigureSubscriber.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright 2025 (C) IDMarinas - All Rights Reserved
+ *
+ * Last modified by "IDMarinas" on 09/12/2025, 16:33
+ *
+ * @project IDMarinas Seo Bundle
+ * @see https://github.com/idmarinas/seo-bundle
+ *
+ * @file SeoConfigureSubscriber.php
+ * @date 01/12/2025
+ * @time 18:08
+ *
+ * @author Iván Diaz Marinas (IDMarinas)
+ * @license BSD 3-Clause License
+ *
+ * @since 1.0.0
+ */
+
+namespace Idm\Bundle\Seo\EventSubscriber;
+
+use Idm\Bundle\Seo\Attributes\Seo;
+use Idm\Bundle\Seo\Attributes\Sitemap;
+use Idm\Bundle\Seo\Service\SeoPageInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ControllerArgumentsEvent;
+
+final readonly class SeoConfigureSubscriber implements EventSubscriberInterface
+{
+	public function __construct (private SeoPageInterface $seo) {}
+
+	public static function getSubscribedEvents (): array
+	{
+		return [
+			ControllerArgumentsEvent::class => 'onControllerArgumentsEvent',
+		];
+	}
+
+	public function onControllerArgumentsEvent (ControllerArgumentsEvent $event): void
+	{
+		$attributes = $event->getAttributes();
+		$routeName = $event->getRequest()->attributes->get('_route', '');
+
+		if (
+			!$event->isMainRequest()
+			|| $event->isPropagationStopped()
+			|| !array_key_exists(Seo::class, $attributes)
+		) {
+			$this->seo->disableSeo();
+
+			return;
+		}
+
+		$locale = $event->getRequest()->attributes->get('_locale', $event->getRequest()->getDefaultLocale());
+
+		$this->seo->setLocale($locale);
+		$this->seo->setRouteName($routeName);
+
+		/* @var Seo $seo */
+		$seo = $attributes[Seo::class][0];
+
+		$entity = array_find($event->getArguments(), fn($arg) => $arg instanceof $seo->entity);
+
+		if (array_key_exists(Sitemap::class, $attributes)) {
+			$this->seo->setSitemap($attributes[Sitemap::class][0]);
+		}
+
+		$this->seo->configure($entity);
+	}
+}

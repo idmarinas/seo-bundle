2ea4e1f79c72dfb8c24c789c63791b15069fecc9
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-09 19:19:01 +0100

    feat(config): add `httpCache` configuration and adjust code style
    - Introduced `httpCache` configuration with `enabled(false)` and `debug(true)` options.
    - Standardized code formatting and style in `framework.php` for consistency.

--- tests/Fixtures/app/config/packages/framework.php
+++ tests/Fixtures/app/config/packages/framework.php
@@ -2,19 +2,19 @@
 /**
  * Copyright 2024-2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 13/11/2025, 14:33
+ * Last modified by "IDMarinas" on 09/12/2025, 19:18
  *
  * @project IDMarinas Seo Bundle
- * @see     https://github.com/idmarinas/seo-bundle
+ * @see https://github.com/idmarinas/seo-bundle
  *
- * @file    framework.php
- * @date    19/03/2025
- * @time    17:06
+ * @file framework.php
+ * @date 19/03/2025
+ * @time 17:06
  *
- * @author  Iván Diaz Marinas (IDMarinas)
+ * @author Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
@@ -32,6 +32,7 @@ return static function (FrameworkConfig $config) {
 		->phpErrors()->log(true)
 	;
 
+	$config->httpCache()->enabled(false)->debug(true);
 	$config->csrfProtection()->enabled(true);
 	$config->propertyAccess()->enabled(true);
 

08395968d6dea784554cbaa493229594572d9baa
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-09 19:11:19 +0100

    feat(cache): add ROUTES_LIST constant to CacheTagEnum and CacheKeyEnum
    - Introduced `ROUTES_LIST` constant to enhance caching capabilities for route lists.

--- src/Cache/CacheKeyEnum.php
+++ src/Cache/CacheKeyEnum.php
@@ -2,26 +2,27 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 08/06/2025, 22:41
+ * Last modified by "IDMarinas" on 08/12/2025, 18:05
  *
  * @project IDMarinas Seo Bundle
- * @see     https://github.com/idmarinas/seo-bundle
+ * @see https://github.com/idmarinas/seo-bundle
  *
- * @file    CacheKeyEnum.php
- * @date    08/06/2025
- * @time    22:38
+ * @file CacheKeyEnum.php
+ * @date 08/06/2025
+ * @time 22:38
  *
- * @author  Iván Diaz Marinas (IDMarinas)
+ * @author Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
 namespace Idm\Bundle\Seo\Cache;
 
 enum CacheKeyEnum: string
 {
-	case SITEMAP = 'idm_seo_sitemap';
+	case SITEMAP     = 'idm_seo_sitemap';
+	case ROUTES_LIST = 'idm_seo_routes_list';
 
 	public function suffix (string $suffix = ''): string
 	{
--- src/Cache/CacheTagEnum.php
+++ src/Cache/CacheTagEnum.php
@@ -2,19 +2,19 @@
 /**
  * Copyright 2025 (C) IDMarinas - All Rights Reserved
  *
- * Last modified by "IDMarinas" on 08/06/2025, 22:53
+ * Last modified by "IDMarinas" on 08/12/2025, 18:05
  *
  * @project IDMarinas Seo Bundle
- * @see     https://github.com/idmarinas/seo-bundle
+ * @see https://github.com/idmarinas/seo-bundle
  *
- * @file    CacheTagEnum.php
- * @date    08/06/2025
- * @time    20:57
+ * @file CacheTagEnum.php
+ * @date 08/06/2025
+ * @time 20:57
  *
- * @author  Iván Diaz Marinas (IDMarinas)
+ * @author Iván Diaz Marinas (IDMarinas)
  * @license BSD 3-Clause License
  *
- * @since   1.0.0
+ * @since 1.0.0
  */
 
 namespace Idm\Bundle\Seo\Cache;
@@ -25,6 +25,7 @@ enum CacheTagEnum: string
 	case SITEMAP_INDEX = 'idm_seo_sitemap_index';
 	case SITEMAP_PAGE  = 'idm_seo_sitemap_page';
 	case SITEMAP_INFO  = 'idm_seo_sitemap_info';
+	case ROUTES_LIST   = 'idm_seo_routes_list';
 
 	public function suffix (string $suffix = ''): string
 	{

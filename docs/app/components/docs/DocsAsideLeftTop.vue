84582a98662a981c32b0a9a02f19d166ab853af4
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-23 18:09:57 +0100

    docs(components): add `DocsAsideLeftTop.vue` for search button and separator
    - Introduced a new component with a collapsible search button and dashed separator for enhanced UI in documentation.

---
+++ docs/app/components/docs/DocsAsideLeftTop.vue
@@ -0,0 +1,12 @@
+<template>
+	<UContentSearchButton
+    :collapsed="false"
+    class="w-full"
+    variant="soft"
+    :ui="{
+      leadingIcon: 'size-4 mr-1',
+    }"
+  />
+
+  <USeparator class="my-6" type="dashed" />
+</template>

a2ba5a5dd67eed186ecdef36f81c73af143bb608
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2025-12-23 18:08:25 +0100

    docs(components): add `LastRelease.vue` for displaying latest release
    - Introduced a Vue component to showcase the latest release as a badge or changelog version.
    - Utilizes `useReleases` composable for fetching release information.

---
+++ docs/app/components/LastRelease.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+const { lastRelease: release } = await useReleases()
+
+defineProps({
+  separator: {
+    type: Boolean,
+    default: false,
+  },
+  asBadge: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <template v-if="release">
+    <USeparator v-if="separator" type="dashed" />
+
+    <UChangelogVersion v-if="!asBadge" v-bind="release" :indicator="false" description="Latest Release"
+      :to="release.path" />
+    <UBadge v-else :label="`Latest Release: ${release.version}`" color="primary" size="lg" icon="i-tabler-tag" />
+  </template>
+</template>

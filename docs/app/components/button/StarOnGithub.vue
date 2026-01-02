b54edcedb5d7a9f07530b5fd238a1d85d4538dfd
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2026-01-02 16:53:12 +0100

    docs(components): remove unused components and clean up related code
    - Deleted obsolete Vue components, including `AppHeaderCenter.vue`, `StarOnGithub.vue`, `Install.vue`, `ConfigBundles.vue`, various `OgImage` components, `Logo.vue`, and `Ship.vue`. These components were no longer relevant or used across the app.

--- docs/app/components/button/StarOnGithub.vue
+++
@@ -1,8 +0,0 @@
-<script lang="ts" setup>
-const bundle = useAppConfig().bundle
-
-</script>
-<template>
-  <UButton icon="i-tabler-brand-github" size="xl" target="_blank" variant="outline" color="neutral"
-    :to="bundle.links.repository">Star on Github</UButton>
-</template>
--- docs/app/components/code/console/Install.vue
+++
@@ -1,9 +0,0 @@
-<script lang="ts" setup>
-const bundle = useAppConfig().bundle
-</script>
-
-<template>
-  <ProsePre language="bash" filename="Terminal">
-    composer require {{ bundle.package_name }}
-  </ProsePre>
-</template>
